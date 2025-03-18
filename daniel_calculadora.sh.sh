60be2faf7a046424dd654fb1a26b5e88d310966a
Danie328 <sdaniel.luis@gmail.com>
2025-03-18 19:36:57 -0300

    Create daniel_calculadora.sh.sh
    Create file daniel_calculadora.sh

---
+++ daniel_calculadora.sh.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+  
+
+#                                                               """"""""""""""" Cálculadora """"""""""""
+
+
+echo 'Olá Professor'
+
+echo o diretório atual é
+
+echo  /home/daniel/modulo1/linux/python
+
+
+echo 'Finalizando o Script'
+
+echo ' Cálcule'
+
+#print = ('digite um número')
+#num1 =  #int(input())
+         #float(input())
+
+#print = ('digite uma operação')
+#operador = #input()
+
+#print = ('digite um segundo número')
+#num2 = #int(input())
+         #float(input())
+
+# if operador == '+':
+ # resultado = num1 + num2
+    #print (resultado)
+
+# elif operador == '-':
+# resultado = num1 - num2
+     #print(resultado)
+
+# elif operador == '*':
+# resultado = num1 * num2
+    #print(resultado)
+
+# elif operador == '/':
+# resultado = num1 / num2
+    #print(resultado)
+
+#  elif operador == '//':
+# resultado = num1 // num2
+     #print(resultado)
+
+# elif operador == '%':
+# resultado = num1 % num2
+    #print(resultado)
+
+# elif operador == '**':
+ # resultado = num1 ** num2
+     #print(resultado)
+
+# else: operadores == <, >, and, not, or:
+  #print ('operação incorreta')
